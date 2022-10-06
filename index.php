1c0f64d9ef84ab6af364027bd39ec9f98cf9b2af
Raziel <chokokokyunokibou@gmail.com>
2022-10-06 08:10:46 -0400

    Added non-functional login page

--- index.php
+++ index.php
@@ -8,18 +8,18 @@
     </head>
     <body class="min-vh-100 m-0 p-0">
         <div class="container-fluid m-0 p-0" id="main-container">
-            <div class="row border vh-100" id="column-with-background">
-                <div class="col-md vh-100">
+            <div class="row border vh-100 m-0 p-0">
+                <div class="col-md vh-100 m-0 p-0">
 		</div>
-		<div class="col-md p-0 vh-100 bg-light">
+		<div class="col-md m-0 p-0 vh-100 bg-light d-flex align-items-center">
 		    <form class="d-flex flex-column p-5 m-0">
 		        <h1 class="fs-3 mb-3 text-center">Charred Manual: A D&D 5e Character Manager</h1>
                         <label for="username" class="text-sm-center text-lg-start">Username</label>
-                        <input class="mx-1 my-2" type="text" id="username" name="username">
+                        <input class="mx-1 my-2 form-control" type="text" id="username" name="username" required>
                         <label for="password" class="text-sm-center text-lg-start">Password</label>
-                        <input class="mx-1 mb-4" type="password" id="password" name="password">
-                        <button class="mt-2 mb-4" type="submit">Login</button>
-		        <p class="text-center mt-1">Don't have an account? <a href="">Register here</a><p>
+                        <input class="mx-1 mb-4 form-control" type="password" id="password" name="password" required>
+                        <button class="btn btn-success mt-2 mb-4" type="submit">Login</button>
+		        <p class="text-center mt-1">Don't have an account? <a href="./signup.php">Register here</a><p>
 		    </form>
                 </div>
             </div>
---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<meta charset="utf-8" content="viewport=device-width;initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<title>Sign-up</title>
+    </head>
+    <body>
+        <div class="d-flex align-items-center vh-100" style="background-image:url(./img/back.jpg);">
+ 	    <div class="container-md border rounded bg-light">
+		<form class="d-flex flex-column mx-5 my-5 px-3" method="POST" action="./classes/controllers/signup.controller.php">
+		    <h1>Create an account</h1>
+	            <label for="username">Username</label>
+		    <input type="text" class="mb-2 form-control" id="username" name="username" required>
+		    <label for="email">E-mail</label>
+		    <input type="email" class="mb-2 form-control" id="email" name="email" required>
+		    <label for="password">Password</label>
+		    <input type="password" class="mb-2 form-control" id="password" name="password" required>
+		    <label for="passwordRepeat">Password (repeat)</label>
+		    <input type="password" class="md-2 form-control" id="passwordRepeat" name="passwordRepeat" required>
+		    <button type="submit" class="btn btn-dark mt-4" >Register</button>
+		    <a href="./index.php" class="d-block mt-2 btn btn-primary text-decoration-none">Return to homepage</a>
+	        </form>
+	    </div>
+        </div>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
