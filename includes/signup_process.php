be36a13279295cc3cc7e487958a6b8477a05fedb
Raziel <chokokokyunokibou@gmail.com>
2022-10-13 15:38:53 -0400

    Partial functionality for signup form, must fix alert spacing

--- includes/signup_process.php
+++ includes/signup_process.php
@@ -1,10 +1,27 @@
 <?php
 
-if(isset($_POST['submit'])){
-	$path = $_SERVER["DOCUMENT_ROOT"]
-	require "includes/classes/models/signup.model.php";
-	require "includes/classes/controllers/signup.controller.php";
-        echo "text<br>";
-        exit();	
-}
+if(isset($_POST['submit'])){	
+	require "./core/dbh.php";
+	require "./classes/models/signup.model.php";
+	require "./classes/controllers/signup.controller.php";
+	
+	$controller = new SignupController();
 
+	if (!$controller->validateUsername($_POST["username"])) {
+		header("Location: ../signup.php?alert=invalidUser");	
+		exit();		
+	}
+
+	if (!$controller->validateEmail($_POST["email"])) {
+		header("Location: ../signup.php?alert=invalidEmail");
+		exit();	
+	}
+
+	if (!$controller->validatePassword($_POST["password"],$_POST["passwordRepeat"])) {
+	    header("Location: ../signup.php?alert=invalidPassword");
+        exit();	
+	}
+
+    header("Location: ../index.php?alert=succesfulRegistration");
+	exit();	
+}
